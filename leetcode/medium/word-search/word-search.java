da7050685b82c0b4c02329159f4fd03cb6de53d6
jstella96 <jstella0530@naver.com>
2021-09-19 20:15:17 +0900

    feat: 리트코드 word-search 자바 풀이 추가

---
+++ leetcode/medium/word-search/word-search.java
@@ -0,0 +1,44 @@
+public class Solution {
+  boolean[][] visited;
+
+  public boolean exist(char[][] board, String word) {
+    if (word.length() == 0)
+      return true;
+
+    int row = board.length;
+    int col = board[0].length;
+    visited = new boolean[row][col];
+
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        if (search(board, word, 0, i, j))
+          return true;
+      }
+    }
+    return false;
+  }
+
+  boolean search(char[][] board, String word, int n, int i, int j) {// n : word의 인덱스
+    if (n == word.length())
+      return true; // 끝까지 돌았으면 .
+    // 끝점이면
+    if (i < 0 || j < 0 || i >= board.length || j >= board[0].length)
+      return false;
+    // 이미 방문했으면
+    if (visited[i][j])
+      return false;
+    // 다르면
+    if (word.charAt(n) != board[i][j])
+      return false;
+
+    // 일치하는 칸을 찾은 경우
+    visited[i][j] = true;
+    boolean result = search(board, word, n + 1, i - 1, j) || search(board, word, n + 1, i + 1, j)
+        || search(board, word, n + 1, i, j - 1) || search(board, word, n + 1, i, j + 1);
+    // 만약 연속된 연산에서 word를 찾지 못하면 해당 칸을 다시 계산해야하므로 false로 갱신해준다.
+    if (result != true)
+      visited[i][j] = false;
+    return result;
+  }
+
+}
